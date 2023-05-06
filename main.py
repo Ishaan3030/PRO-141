d1ece8c5941436b4c50856ec2a6452b668aa8440
Ishaan3030 <86636860+Ishaan3030@users.noreply.github.com>
2023-05-06 17:38:57 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request
+from demographic_filtering import output
+from content_filtering import get_recommendations
+import pandas as pd
+
+movies_data = pd.read_csv('final.csv')
+
+app = Flask(__name__)
+
+all_movies = movies_data[["original_title","poster_link","release_date","runtime","weighted_rating"]]
+
+liked_movies = []
+not_liked_movies = []
+did_not_watch = []
+
+def assign_val():
+    m_data = {
+        "original_title": all_movies.iloc[0,0],
+        "poster_link": all_movies.iloc[0,1],
+        "release_date": all_movies.iloc[0,2] or "N/A",
+        "duration": all_movies.iloc[0,3],
+        "rating":all_movies.iloc[0,4]/2
+    }
+    return m_data
+
+@app.route("/get-article")
+def get_article():
+   
+   return 'Write code to display teh first item in all_articles list'
+
+@app.route("/liked-article", methods=["POST"])
+def liked_article():
+   
+   return 'Write code to shift first article into liked_articles list'
+
+@app.route("/unliked-article", methods=["POST"])
+def unlike_article():
+   
+   return 'Write code to shift first article into not_liked_articles list'
+
+if __name__ == "__main__":
+  app.run()
